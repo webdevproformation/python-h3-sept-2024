75f3af5be0b7443f62d083bbca7d1646aa69afa2
Malik H <malik.h@webdevpro.net>
2024-09-16 16:44:33 +0200

    énoncé

---
+++ jour01/07-exo.py
@@ -0,0 +1,32 @@
+"""
+créer un nouveau fichier .py
+1/ créer la variable test qui va contenir 
+le résultat de la comparaison suivante : 255 <= 3
+2/ créer la variable inconnu de type texte contenant le mot 'bonjour'
+3/ créer la variable voiture qui est un dictionnaire vide
+4/ afficher ces trois variables dans le terminal
+"""
+test = 255 <= 3
+inconnu = "bonjour"
+voiture = {}
+
+print(test)
+print(inconnu)
+print(voiture)
+
+# python 07-exo.py
+
+"""
+1 créer un nouveau fichier .py
+2 ce fichier contient trois variables
+titre = "M"
+prenom = "John"
+nom = "Doe"
+
+3 écrire la phrase suivante dans la console en utilisant les 3 variables de 2 manières possibles :
+
+Je m'appelle Monsieur John Doe
+
+concaténation en Python
+
+"""
