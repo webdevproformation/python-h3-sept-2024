2767957aa63b225173086c4fa63b8d17a8476c13
Malik H <malik.h@webdevpro.net>
2024-09-17 09:52:21 +0200

    enoncé

---
+++ jour02/02-chiffre.py
@@ -0,0 +1,31 @@
+compteur = 1 
+
+compteur = compteur + 1
+
+print(compteur) # 2
+
+compteur += 1 
+
+print(compteur) # 3
+
+# compteur++ l'opérateur uniaire ++ n'existe pas  
+# ++compteur l'opérateur uniaire ++ n'existe pas  
+
+compteur -= 1
+compteur /= 1
+compteur *= 1
+
+age = 10 ** 2 # puissance
+print(age) # 100
+
+taille = 5 
+print (taille % 2) # 2 * 2 + 1 # reste entier
+print (taille / 2) # à voir partir entière de la division
+
+# changement de type
+
+# str()
+# int()
+# list()
+# tuple()
+# set()
---
+++ jour02/03-condition.py
@@ -0,0 +1,78 @@
+# condition 
+
+""" if (2 > 3){
+
+} """
+
+if 2 > 1 :
+    # marque le début d'un bloc de code marqué par l'indentation
+    # commentaire
+    pass 
+
+
+if 2 > 1 :
+    a = 10 
+
+"""
+>
+<
+== => égalité stricte => valeur ET type 
+!=
+>=
+<= 
+
+ça n'existe en python mais existe en PHP en JS
+===
+!==
+
+"""
+
+"""
+en JS ou en PHP
+if("2" == 2){ # true 
+if("2" == "2"){ # true 
+if(2.0 == 2){ # true 
+
+}
+if("2" === 2){ # false
+
+}
+"""
+
+# verifier que age compris entre 0 et 20
+
+"""
+js 
+const age = 15
+if(age > 0 && age < 10){
+}
+
+Python 
+
+and 
+or 
+not 
+=> attention en minuscule 
+
+n'existe pas en Python
+&&
+||
+!
+
+"""
+
+"""
+créer le fichier 04-exo.py 
+pour chaque comparaison dire si le résultat est True ou False
+
+2 >= 2
+"a" == "ab"
+2 != 3 and 10 < 33
+"hello" > "bonjour" 
+2 ==  "2"
+"2" == "2"
+(2 != 5 and 3 > 4 ) or 2 <= 14 
+2 != 5 and ( 3 > 4  or 2 <= 14 ) 
+2 != 5 and 3 > 4  or 2 <= 14  
+
+"""
