f71cb11a147437f5751cf2d68c655413a20c2f8e
Malik H <malik.h@webdevpro.net>
2024-09-17 10:09:11 +0200

    correction

---
+++ jour02/04-exo.py
@@ -0,0 +1,28 @@
+"""
+
+2 >= 2           True
+"a" == "ab"        False
+2 != 3 and 10 < 33 True
+"hello" > "bonjour"  True 
+2  ==  "2"    False 
+"2" == "2"    True
+(2 != 5 and 3 > 4 ) or 2 <= 14 
+(  True  and False ) or True
+        False        or True
+        True
+2 != 5 and ( 3 > 4  or 2 <= 14 ) 
+True   and ( False  or   True  )
+True   and     True
+True   
+
+2 != 5 and 3 > 4  or 2 <= 14  
+True   and False  or True
+   False          or True
+   True
+
+"""
+
+print("hello" > "bonjour") # True 
+
+# h => 68
+# b => 62
