d7744d6d0cf3a7e8de9123fe91ac8b8c4d8faaf9
Malik H <malik.h@webdevpro.net>
2024-09-18 14:14:39 +0200

    enoncé

--- jour03/06-tuple.py
+++ jour03/06-tuple.py
@@ -52,10 +52,10 @@ créer un nouveau fichier 07-exo.py
 dans ce fichier vous avez une liste de produits
 
 produits = [
-    # id   nom    prix  dispo  qte
-    ( 1 , "PS4" , 200, True , 2),
-    ( 2 , "PS5" , 600, False  , 3),
-    ( 3 , "Nintendo DS" , 400, True , 5 ),
+    # id   nom            prix  dispo  qte
+    ( 1 , "PS4"         , 200, True   , 2),
+    ( 2 , "PS5"         , 600, False  , 3),
+    ( 3 , "Nintendo DS" , 400, True   , 5 ),
 ]
 
 - à partir de cette liste de tuple vous devez calculer le total de la commande passée pour tous les produits qui sont True
