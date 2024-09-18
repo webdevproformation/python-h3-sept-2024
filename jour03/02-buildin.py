9c96803981c32a71b96c31c2566f4558b974aa41
Malik H <malik.h@webdevpro.net>
2024-09-18 10:52:06 +0200

    builtins

---
+++ jour03/02-buildin.py
@@ -0,0 +1,17 @@
+# les fonctions builtins
+
+import builtins
+
+# par rapport à d'autres langages => il n'y a QUE 60 fonctions builtins 
+# 60 fonctions sont utilisables QUELQUESOIT LE TYPE 
+# len() => __len__
+
+# range()
+# print()
+# int()
+# list()
+# tuple()
+# str()
+# zip()
+# enumerate()
+# type()
