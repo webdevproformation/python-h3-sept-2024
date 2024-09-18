038dd9a6b22702d14e4217ff68d9ea6eadc81ac5
Malik H <malik.h@webdevpro.net>
2024-09-18 14:03:59 +0200

    commit

--- jour03/06-tuple.py
+++ jour03/06-tuple.py
@@ -52,13 +52,13 @@ créer un nouveau fichier 07-exo.py
 dans ce fichier vous avez une liste de produits
 
 produits = [
-    #             prix        qte
+    # id   nom    prix  dispo  qte
     ( 1 , "PS4" , 200, True , 2),
     ( 2 , "PS5" , 600, False  , 3),
     ( 3 , "Nintendo DS" , 400, True , 5 ),
 ]
 
-- à partir de cette liste de tuple vous devez calculer le total de la commande passée
+- à partir de cette liste de tuple vous devez calculer le total de la commande passée pour tous les produits qui sont True
 - pouvez vous changer la valeur de False pour la PS5 en True ? (vous devrez passer par une list pour pouvoir effectuer cette modification)
 
 
