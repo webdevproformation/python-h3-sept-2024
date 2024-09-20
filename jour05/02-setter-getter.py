82c1c8ee4dd9f3e1b9ff230cf33045999ac1c479
Malik H <malik.h@webdevpro.net>
2024-09-20 10:57:07 +0200

    coquilles

--- jour05/02-setter-getter.py
+++ jour05/02-setter-getter.py
@@ -63,7 +63,7 @@ print(vars(e))
 
 # créer les setter et getter pour ces deux propriétés
 # nom => obligatoire une string de minimum 5 lettres
-# sujet => obligatoirement un liste qui contient au moins 2 valeurs 
+# sujets => obligatoirement une liste qui contient au moins 2 valeurs 
 
 # utiliser créer des  setter et getter qui vont garantir les valeurs associées à ces propriétés sont conformes à vos attentes 
 
